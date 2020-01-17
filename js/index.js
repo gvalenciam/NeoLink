cf1fb79ab5f269bd51d0b95be456808556b51900
gvalenciam <gvalenciam@pucp.pe>
2020-01-16 23:23:19 -0500

    Se agrego sidebar y grafico prueba d3

--- js/index.js
+++ js/index.js
@@ -1,5 +1,26 @@
+var admin = {
+  usuario: "neolink@neolink.com",
+  password: "neolink123"
+};
+
 $(document).ready(function() {
-  document.getElementById("formulario-log-in").onsubmit = logIn;
+  document
+    .getElementById("formulario-log-in")
+    .addEventListener("click", function(event) {
+      event.preventDefault();
+      logIn(admin);
+    });
 });
 
-function logIn() {}
+function logIn(usuario) {
+  if (
+    usuario.usuario.localeCompare("neolink@neolink.com") === 0 &&
+    usuario.password.localeCompare("neolink123") === 0
+  ) {
+    console.log("OK");
+    location.href = "dashboard.html";
+  } else {
+    console.log("ERROR");
+    //ERROR
+  }
+}
