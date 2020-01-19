ce965dd0b82557f81b036b96e88ecadfdc419a8f
citaUtec <cita@utec.edu.pe>
2020-01-18 23:54:05 -0500

    logica para mostrar secciones ok

--- js/dashboard.js
+++ js/dashboard.js
@@ -7,4 +7,27 @@ $(document).ready(function() {
       $(".titulo-navbar").toggleClass("titulo-navbar-activo");
       $(".boton-sidebar").toggleClass("boton-sidebar-activo");
     });
+
+  var secciones = Array.from(
+    document.querySelectorAll(".contenido-seccion .seccion")
+  );
+  var menus = Array.from(document.querySelectorAll(".lista-menus .menu"));
+
+  menus.forEach(function(menu, index) {
+    if (!menu.classList.contains("menu-activo"))
+      $("#" + secciones[index].id).hide();
+    menu.addEventListener(
+      "click",
+      function() {
+        menus.forEach(function(elemento) {
+          elemento.classList.remove("menu-activo");
+        });
+        secciones.forEach(function(seccion) {
+          $("#" + seccion.id).fadeOut(200);
+        });
+        menu.classList.add("menu-activo");
+        $("#" + secciones[index].id).fadeIn(200);
+      }.bind(index)
+    );
+  });
 });
